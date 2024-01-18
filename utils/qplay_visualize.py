2f4a1d94f49f6b7591abaa13359c61d8d6433711
jenkinsm13 <100166407+jenkinsm13@users.noreply.github.com>
2024-01-17 16:45:19 -0800

    Create qplay_visualize.py

---
+++ utils/qplay_visualize.py
@@ -0,0 +1,14 @@
+# /utils/qplay_visualize.py
+
+from qplay_core import *
+import matplotlib.pyplot as plt
+
+def plot_state(registers):
+    # Visualize quantum state
+    fig, ax = plt.subplots()
+    pass
+
+def plot_circuit(circuit):
+   # Visualize a quantum circuit
+   plt.gcf().clear()
+   pass
